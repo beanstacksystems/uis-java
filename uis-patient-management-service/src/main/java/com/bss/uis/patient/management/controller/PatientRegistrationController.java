10fea1bf2908f2281b301188451a21a3738d1dc0
Amit <mail2amit.saha@gmail.com>
2021-02-19 20:50:28 +0530

    Liquibase config

--- uis-patient-management-service/src/main/java/com/bss/uis/patient/management/controller/PatientRegistrationController.java
+++ uis-patient-management-service/src/main/java/com/bss/uis/patient/management/controller/PatientRegistrationController.java
@@ -8,10 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.Serializable;
-
 @RestController
-public class PatientRegistrationController implements Serializable {
+public class PatientRegistrationController {
 
     @Value("${server.url:Unable to connect to config server}")
     private String url;
