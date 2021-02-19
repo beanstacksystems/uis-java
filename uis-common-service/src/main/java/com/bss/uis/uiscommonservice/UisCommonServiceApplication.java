8d41cdeea6d4b31e34fc0744057cebb2216d95e2
Amit <mail2amit.saha@gmail.com>
2021-02-19 21:42:59 +0530

    Added Common Service

---
+++ uis-common-service/src/main/java/com/bss/uis/uiscommonservice/UisCommonServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bss.uis.uiscommonservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UisCommonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UisCommonServiceApplication.class, args);
+	}
+
+}
---
+++ uis-common-service/src/test/java/com/bss/uis/uiscommonservice/UisCommonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bss.uis.uiscommonservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UisCommonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
